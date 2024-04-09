35dc1454f2381892dd5d161efba4bd6c71201867
lklein <lars.klein@epfl.ch>
2024-04-09 15:44:11 +0200

    adding tests

--- evaluation/gsm8k/compare_with_reference.py
+++ evaluation/gsm8k/compare_with_reference.py
@@ -6,6 +6,8 @@ def judge(model_outputs: List[str], ground_truths:List[str]):
     assert len(model_outputs) == len(ground_truths)
     results = []
     for model_output, ground_truth in zip(model_outputs, ground_truths):
-        results.append(is_correct(model_output, ground_truth))
+        assert "answer" in ground_truth, "The ground truth should have an answer key"
+        assert "answer" in model_output, "The model output should have an answer key"
+        results.append(is_correct(model_output["answer"], ground_truth))
 
     return results
