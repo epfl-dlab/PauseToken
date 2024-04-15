9ea21741bbfe3ba349f9a1b3f1b99a924e9b17cc
lklein <lars.klein@epfl.ch>
2024-04-15 12:43:06 +0200

    wip: adding small training setup for testing

--- notebooks/embedding_wrapper.py
+++ notebooks/embedding_wrapper.py
@@ -79,6 +79,29 @@ class EmbeddingWrapper2(nn.Module):
             return y
 
 
+class EmbeddingWrapper3(nn.Module):
+    def __init__(self, old_embedding: nn.Embedding, num_embeddings: int, freeze_old=True):
+        super().__init__()
+        self.embedding = nn.Embedding(num_embeddings, old_embedding.embedding_dim)
+        self.embedding.to(old_embedding.weight.device).to(old_embedding.weight.dtype)
+        self.embedding.weight.data[:old_embedding.num_embeddings] = old_embedding.weight.data
+
+        # ToDo: I don't think this has any effect
+        if freeze_old:
+            for param in old_embedding.parameters():
+                param.requires_grad = False
+
+        # register hook to update gradients
+        self.num_old_embeddings = old_embedding.num_embeddings
+        self.embedding.register_backward_hook(self._hook)
+
+    def _hook(self, grad):
+        # zero out the gradient for the embedding weights coming from the old embedding
+        grad[:self.num_old_embeddings].zero_()
+
+    def forward(self, x):
+        return self.embedding(x)
+
 class Llama2EmbeddingSurgeon():
     def __init__(self, llama, extended_tokenizer):
         self.llama = llama
@@ -158,3 +181,96 @@ class PeftModelEmbeddingSurgeon():
         surgeon.extended_embedding.load_state_dict(extended_embedding_dict)
         surgeon.extended_unembedding.load_state_dict(extended_unembedding_dict)
         return surgeon
+
+
+if __name__=="__main__":
+    # set up a simple model and run a few training steps, ensure that the old embeddings are not changed
+    # and the new embeddings are changed
+
+    # set up a simple model
+    model = AutoModelForCausalLM.from_pretrained("gpt2")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    extended_tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    extended_tokenizer.add_special_tokens({"additional_special_tokens": ["<|NEW_TOKEN|>"]})
+
+    # set up the surgeon
+    extended_embedding = EmbeddingWrapper3(model.base_model.wte, len(extended_tokenizer))
+    extended_unembedding = LinearWrapper(model.lm_head, len(extended_tokenizer))
+
+    model.base_model.wte = extended_embedding
+    model.lm_head = extended_unembedding
+    model.config.vocab_size = len(extended_tokenizer)
+
+    from datasets import load_dataset
+
+    dataset = load_dataset("gsm8k", "main", split="train")
+    alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+    ### Instruction:
+    {}
+
+    ### Input:
+    {}
+
+    ### Response:
+    {}"""
+
+    EOS_TOKEN = tokenizer.eos_token  # Must add EOS_TOKEN
+
+
+    def formatting_prompts_func(examples):
+        instructions = len(examples["question"]) * [
+            "Solve the math problem using a eval tool. The command eval[[expr]] allows you to evaluate an expression."]
+        inputs = examples["question"]
+        outputs = examples["answer"]
+        texts = []
+        for instruction, input, output in zip(instructions, inputs, outputs):
+            # Must add EOS_TOKEN, otherwise your generation will go on forever!
+            text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+            texts.append(text)
+        # print(texts)
+        return {"text": texts, }
+
+    dataset = dataset.map(formatting_prompts_func, batched=True)
+
+    w_before = model.base_model.wte.embedding.weight.detach().cpu().clone()
+
+    from trl import SFTTrainer
+    from transformers import TrainingArguments
+    import os
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    trainer = SFTTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        train_dataset=dataset,
+        eval_dataset=None,
+        dataset_text_field="text",
+        max_seq_length=1024,
+        dataset_num_proc=2,
+        packing=False,  # Can make training 5x faster for short sequences.
+        args=TrainingArguments(
+            gradient_checkpointing=False,
+            per_device_train_batch_size=1,
+            gradient_accumulation_steps=1,
+            warmup_steps=0,
+            max_steps=2,
+            # num_train_epochs = 1,
+            learning_rate=2e-3,
+            logging_steps=1,
+            optim="adamw_torch",
+            weight_decay=0.01,
+            # lr_scheduler_type = "linear",
+            seed=3407,
+            output_dir="outputs",
+        ),
+    )
+
+    trainer_stats = trainer.train()
+
+    # compare w_before and w_after
+    w_after = model.base_model.wte.embedding.weight.detach().cpu().clone()
+    print(w_before.shape, w_after.shape)
+    print(w_before, w_after)
+    assert torch.allclose(w_before, w_after[:w_before.shape[0]])
