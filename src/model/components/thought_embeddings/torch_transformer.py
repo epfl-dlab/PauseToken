f82cb3c6029329068d26e1e06fa7ca42a7fa6623
Nicolas Baldwin <nicolas.mario.baldwin@gmail.com>
2025-03-18 19:22:34 +0100

    slight bug fix

--- src/model/components/thought_embeddings/torch_transformer.py
+++ src/model/components/thought_embeddings/torch_transformer.py
@@ -10,6 +10,7 @@ class ThoughtTransformer(torch.nn.Module):
         else:
             self.model = transformer_config
         self.hidden_dim = torch.tensor(hidden_dim, requires_grad=False)
+        self.divide_by_sqrt_dim = divide_by_sqrt_dim
 
 
     def forward(self, last_hidden_states: torch.Tensor, attention_mask=None):
@@ -18,7 +19,7 @@ class ThoughtTransformer(torch.nn.Module):
         
         divide = torch.sqrt(self.hidden_dim) if self.divide_by_sqrt_dim else self.hidden_dim        
         thoughts = self.model(inputs_embeds=last_hidden_states, attention_mask=attention_mask).last_hidden_state / divide
-        
+                
         return thoughts
     
 
