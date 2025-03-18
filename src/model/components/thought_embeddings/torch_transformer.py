4050f047e6e70e915678ded7abcc7d22a9b88e84
Nicolas Baldwin <nicolas.mario.baldwin@gmail.com>
2025-03-18 19:16:33 +0100

    add different division methods in thought perturbance

--- src/model/components/thought_embeddings/torch_transformer.py
+++ src/model/components/thought_embeddings/torch_transformer.py
@@ -3,7 +3,7 @@ import hydra
 
 class ThoughtTransformer(torch.nn.Module):
     def __init__(
-            self, transformer_config, hidden_dim):
+            self, transformer_config, hidden_dim, divide_by_sqrt_dim=False):
         super(ThoughtTransformer, self).__init__()
         if isinstance(transformer_config, dict):
             self.model = hydra.utils.instantiate(transformer_config)
@@ -15,8 +15,9 @@ class ThoughtTransformer(torch.nn.Module):
     def forward(self, last_hidden_states: torch.Tensor, attention_mask=None):
         if attention_mask is None:
             attention_mask = torch.ones((last_hidden_states.size(0), last_hidden_states.size(1)), device=last_hidden_states.device)
-            
-        thoughts = self.model(inputs_embeds=last_hidden_states, attention_mask=attention_mask).last_hidden_state / torch.sqrt(self.hidden_dim)
+        
+        divide = torch.sqrt(self.hidden_dim) if self.divide_by_sqrt_dim else self.hidden_dim        
+        thoughts = self.model(inputs_embeds=last_hidden_states, attention_mask=attention_mask).last_hidden_state / divide
         
         return thoughts
     
