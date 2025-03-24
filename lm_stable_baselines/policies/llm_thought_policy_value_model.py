fix in computation of entropy

lm_stable_baselines/policies/llm_thought_policy_value_model.py
@@ -268,7 +268,7 @@ class LLMThoughtPolicyValueModel(LLMBasePolicyValueModel):
                 obs_mask[i, action_start_indices[i]:] = 0
        
         values = self.value_forward_pass(raw_latent, obs_mask)
-        entropy = - (log_probs * log_probs.exp()).sum(dim=-1).mean()
+        entropy = - (log_probs * log_probs.exp()).mean()
         return values, log_probs, entropy
   
 